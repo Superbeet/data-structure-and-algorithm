19a6bb93c1859e3423f167c6072afcc2e178068e
Superbeet <aslan.yeh2010@gmail.com>
2015-08-21 16:23:43 -0700

    update

--- CC150/Chapter1-1.6.py
+++ CC150/Chapter1-1.6.py
@@ -8,6 +8,7 @@
 5 a b c d e f
 6 a b c d e f
 """
+import copy
 
 def print_matrix(target_matrix):
 	x_axis = [ str(i) for i in range(0, len(target_matrix)+1) ]
@@ -19,19 +20,56 @@ def print_matrix(target_matrix):
 		row_items = ' '.join(row)
 		print "%d %s" %(row_num, row_items)
 
-def rotate_image(target_str):
-	if len(target_str):
-		pass
+def rotate_image(target_matrix):
 
+    start_level = 0
+
+    end_level = len(target_matrix)/2
+
+    start_index = 0
+##    end_index = end_level - start_index
+
+    for level in range(start_level , end_level-start_level):
+
+        start_index = level
+
+        end_index = len(target_matrix) - start_index - 1
+
+        print "[start_level, end_level, level][start_index, end_index] -> %s%s"\
+            %([start_level, end_level, level],[start_index, end_index])
+
+        top = copy.deepcopy(target_matrix[level][start_index:end_index])
+        print "top ->", top
+
+        for index in range(start_index, end_index-start_index):
+
+            target_matrix[level][index] = target_matrix[end_index-index][level]
+            target_matrix[index][level] = target_matrix[end_level-level][index]
+            target_matrix[end_level][index] = target_matrix[end_index-index][end_level]
+            target_matrix[index][end_level-level] = top[index]
+
+        top = []
+
+        print_matrix(target_matrix)
+
+    return target_matrix
 
 if __name__ == '__main__':
-	my_matrix = [
-		['a','b','c','d','e','f'],
-		['a','b','c','d','e','f'],
-		['a','b','c','d','e','f'],
-		['a','b','c','d','e','f'],
-		['a','b','c','d','e','f'],
-		['a','b','c','d','e','f'],
-	]
+    my_matrix = [
+            		['a','b','c','d','e','f'],
+            		['a','b','c','d','e','f'],
+            		['a','b','c','d','e','f'],
+            		['a','b','c','d','e','f'],
+            		['a','b','c','d','e','f'],
+            		['a','b','c','d','e','f'],
+            	]
 
-	print_matrix(my_matrix)
+    print_matrix(my_matrix)
+
+    print "----------------------"
+
+    new_matrix = rotate_image(my_matrix)
+
+    print "----------------------"
+
+    print_matrix(new_matrix)

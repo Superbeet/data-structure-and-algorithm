a78d3c46d570e7e1c8c9fc4386a960cd32894eca
jeffrey <aslan.yeh2010@gmail.com>
2015-08-25 00:44:37 -0700

    Chapter 2 - 2.1

--- CC150/Chapter2-2.1.py
+++ CC150/Chapter2-2.1.py
@@ -32,10 +32,10 @@ class UnorderedLinkedList(object):
         last_node = None
         node = None
 
-        for i in range(0,10):
+        for i in range(0,30):
             node = SinglyLinkedListNode()
 
-            node.data = str(i + 1)   # assign some values
+            node.data = str(i % 10 + 1)   # assign some values
 
             if i is 0:
 
@@ -60,7 +60,7 @@ class UnorderedLinkedList(object):
         while(select_node):
 ##            print "round %d - %s" %(i, select_node.data)
 ##            i = i + 1
-            print select_node.data
+            # print select_node.data
 
             data_list.append(str(select_node.data))
 
@@ -71,22 +71,108 @@ class UnorderedLinkedList(object):
 def remove_duplicate(head_node):
 
     if head_node is None:
-        print "Linked List is NULL"
+        print "Linked list is NULL"
         return False
 
     hash_list = []
+
+    last_node = None
 
     select_node = head_node
 
     while(select_node):
 
-        if select_node.next.data not in hash_list:
+        # print hash_list, select_node.data
+
+        if select_node.data not in hash_list:
+
             hash_list.append(select_node.data)
 
+            last_node = select_node
+
         else:
-            select_node.next = select_node.next.next
+
+            last_node.next = select_node.next
 
         select_node = select_node.next
 
+    return head_node
+
+def remove_duplicate_no_buffer_2(head_node):
+    """ Use two pointers
+        one extra pointer
+    """
+
+    if head_node is None:
+        print "Linked list is NULL"
+        return False
+
+    select_node = head_node
+
+    while(select_node):
+
+        compare_node = select_node.next
+
+        while(compare_node):
+
+            last_node = select_node
+
+            if compare_node.data == select_node.data:  
+                # print "     remove_node->",compare_node.data
+                # print "     compare_node.next->",compare_node.next
+                # print "     last_node->", last_node.data
+                last_node.next = compare_node.next
+
+            else:
+
+                last_node = compare_node
+
+            compare_node = compare_node.next
+
+        select_node = select_node.next
+
+def remove_duplicate_no_buffer_1(head_node):
+    """ Use two pointers
+        no extra space
+    """
+
+    if head_node is None:
+        print "Linked list is NULL"
+        return False
+
+    select_node = head_node
+
+    while select_node:
+
+        runner = select_node
+
+        while runner.next:
+
+            if runner.next.data == select_node.data:
+
+                runner.next = runner.next.next
+
+            else:
+
+                runner = runner.next
+
+        select_node = select_node.next
+
+    return head_node
+
 if __name__ == '__main__':
-    main()
+
+    ll = UnorderedLinkedList()
+
+    head = ll.initialize()
+
+    ll.traverse()
+
+    # remove_duplicate(head)
+
+    # ll.traverse()
+
+    # remove_duplicate_no_buffer_1(head)
+    remove_duplicate_no_buffer_2(head)
+
+    ll.traverse()

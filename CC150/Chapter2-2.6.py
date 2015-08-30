ff81cab3bb7539c8643eae7d1985bec863f93c67
Jeffrey Ye <jeffrey.congye@gmail.com>
2015-08-29 17:45:01 -0700

    Chapter 2 Question 2.6

--- CC150/Chapter2-2.6.py
+++ CC150/Chapter2-2.6.py
@@ -106,11 +106,68 @@ def traverse(head, loop_point = None):
 
         select_node = select_node.next
 
-    print "==>".join(data_list)
+    print "=>".join(data_list)
+
+def check_loop(head):
+
+    if head is None or head.next is None:
+        return False
+
+    fast_runner = head
+    slow_runner = head
+
+    while fast_runner is not None and fast_runner.next is not None:
+        slow_runner = slow_runner.next
+        fast_runner = fast_runner.next.next
+
+        if fast_runner is slow_runner:
+            break
+
+    if fast_runner is slow_runner:
+        return fast_runner
+
+    else:
+        return False
+
+def find_loop_beginning(head):
+
+    if head is None or head.next is None:
+        return False
+
+    collision_node = check_loop(head)
+
+    if collision_node:
+        front_runner = head
+        rear_runner = collision_node
+
+        while front_runner is not rear_runner:
+
+            front_runner = front_runner.next
+            rear_runner = rear_runner.next
+
+        return front_runner
+
+    else:
+        return False
+
 
 if __name__ == '__main__':
     ll = UnorderedLinkedList()
+
     head, loop_point = ll.initialize_loop_ll()
     traverse(head, loop_point)
+
+    # head = ll.initialize_ll()
+    # traverse(head)
+
+    # loop_exsit = check_loop(head)
+    # if loop_exsit:
+    #     print "loop is existing"
+    # else:
+    #     print "loop is not existing"
+
+    loop_beginning = find_loop_beginning(head)
+    if loop_beginning:
+        print "loop beginning point -> %s" %(loop_beginning.data)
 
 
